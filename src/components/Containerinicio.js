532bb666e6d2d239b06582d777212952b7b00b70
JuanGuzmanH <juand.guzmanh@utadeo.edu.co>
2021-05-03 18:29:52 -0500

    asd

--- src/components/Containerinicio.js
+++ src/components/Containerinicio.js
@@ -1,11 +1,9 @@
 import React from 'react'
-<<<<<<< HEAD
+
 import { Route } from 'react-router'
 
-=======
-import { Route, Switch } from 'react-router'
+import { Switch } from 'react-router'
 import '../assets/css/Maincontainer.css'
->>>>>>> c92dfa07f94a78494d0f53acc2524c5f061b272a
 import { BrowserRouter as Router } from 'react-router-dom'
 import { FormRegistro } from './FormRegistro'
 import { FormInicio } from './FormInicio'
