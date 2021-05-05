f2827fced0d0047497ab44e236d4fcee0124aab2
Michael Rivera <michaela.riverac@utadeo.edu.co>
2021-05-05 14:24:22 -0500

    corrigiendo error class y for

--- src/components/Storymenu.js
+++ src/components/Storymenu.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Storycard } from './Storycard'
 import '../assets/css/Storymenu.css'
 
+import {url} from '../components/utilities/APIurl'
+
 export const Storymenu = () => {
+    const [historias, setHistorias] = useState([]);
+    const getHistorias=async()=>{
+        const listar= url+"publicaciones/listar_publicaciones";
+        const resp = await fetch(listar);
+        const historias = await resp.json();
+        const {publicaciones} = historias
+        setHistorias(publicaciones);
+    }
+    useEffect(() => {
+        getHistorias();
+    }, [])
+    console.log(historias)
     return (
         <div id="main-feed">
             <h1> Bienvenido</h1>
             <h1> Usuario</h1>
             <div id="galeria-inicio">
                 <ul>
-                    <li>
-                        <Storycard />
-                    </li>
+                    {historias.map(historia=>(
+                        <h1>{historia._id}</h1>
+                    ))}
                 </ul>
             </div>
 
