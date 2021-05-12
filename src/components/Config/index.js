6d8d9bf39699a4a38cc7f39c4ad3836e1152d6e7
Steveen Dominguez <steveena.dominguezb@utadeo.edu.co>
2021-05-12 16:40:23 -0500

    empezondo configuracion

--- src/components/Config/index.js
+++ src/components/Config/index.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './index.css'
 import logo from '../../assets/images/logo.png'
+import {url} from '../utilities/APIurl'
 
 export const Config = () => {
+    const [nombre, setnombre] = useState("")
+    const [usuario, setUsuario] = useState({})
+    const getSesion = async()=>{
+        let url1 = url + "sesion/ver_sesion";
+        let resp = await (await(fetch(url1))).json()
+        const em = resp.sesion.correo
+
+        url1 = url + "usuarios/buscar_usuario_por_correo/" + em
+        resp = await (await(fetch(url1))).json()
+        setUsuario(resp.usuario)
+        const id = usuario._id;
+        console.log(usuario);
+    }
+
+    useEffect(() => {
+        getSesion()
+    }, [])
     return (
         <div id="cont-config">
             <div id="panel-config">
                 <div className="cont" id="cont-user">
                     <div id="cont-img">
-                        <button id="btn-img"><img id="img-user" src={logo} width="100px" height="100px" alt="" /></button>
+                        <button id="btn-img"><img id="img-user" src={usuario.url_foto_perfil} width="100px" height="100px" alt="" /></button>
                     </div>
                     <div id="cont-nomUser">
-                        <h1>Nombre Usuario</h1>
+                        <h1>{usuario.usuario}</h1>
                     </div>
                 </div>
                 <div id="label-info-personal">
@@ -23,7 +41,7 @@ export const Config = () => {
                             <h2 className="label-config">Nombre: </h2>
                         </div>
                         <div id="input-nom">
-                            <input className="input-config" type="text" defaultValue="Nombre" />
+                            <input className="input-config" type="text" defaultValue={usuario.nombre} />
                         </div>
                     </div><br />
                     <div className="cont" id="cont-nom_User">
@@ -31,7 +49,7 @@ export const Config = () => {
                             <h2 className="label-config">Nombre usuario: </h2>
                         </div>
                         <div id="input-nom_User">
-                            <input className="input-config" type="text" defaultValue="Nombre usuario" />
+                            <input className="input-config" type="text" defaultValue={usuario.usuario} onChange={({target: {value}})=> setnombre(value)} />
                         </div>
                     </div><br />
                     <div className="cont" id="cont-config-correo">
@@ -39,7 +57,7 @@ export const Config = () => {
                             <h2 className="label-config">Correo: </h2>
                         </div>
                         <div id="input-correo">
-                            <input className="input-config" type="text" placeholder="Correo" />
+                            <input className="input-config" type="text" defaultValue={usuario.correo} />
                         </div>
                     </div>
                     <div className="cont-button">
